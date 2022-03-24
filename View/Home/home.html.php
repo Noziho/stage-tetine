0eea123874b9fa189c7be5300ab8303deb0f742a
noahd <noah.decroix3@gmail.com>
2022-03-24 11:54:09 +0100

    add carousel form home page

--- View/Home/home.html.php
+++ View/Home/home.html.php
@@ -1,2 +1,17 @@
+<div class="swiper">
+    <!-- Additional required wrapper -->
+    <div class="swiper-wrapper">
+        <!-- Slides -->
+        <div class="swiper-slide" ><img src="/assets/img/carousel/first-image.jpg" alt=""></div>
+        <div class="swiper-slide"><img src="/assets/img/category/Disney/disney(1).jpg" alt=""></div>
+        <div class="swiper-slide">Slide 3</div>
+    </div>
+    <!-- If we need pagination -->
+    <div class="swiper-pagination"></div>
+
+    <!-- If we need navigation buttons -->
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>
 
 
+</div>
