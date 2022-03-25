aa0c30a7ea2e7dfb4b6c01e0f7542de7cfbec2c7
noahd <noah.decroix3@gmail.com>
2022-03-25 11:49:19 +0100

    edit add basket product

--- Model/Manager/BasketManager.php
+++ Model/Manager/BasketManager.php
@@ -10,16 +10,18 @@ class BasketManager  extends AbstractManager
 {
     private const TABLE = 'mdf58_basket';
 
-    public static function addProductToBasket (int $productId, int $quantity)
+    public static function addProductToBasket (int $productId, int $quantity, int $price, int $user)
     {
 
         $stmt = DB_Connect::dbConnect()->prepare("
-            INSERT INTO " . self::TABLE . " (quantity, product_fk) 
-            VALUES (:quantity, :product_fk)
+            INSERT INTO " . self::TABLE . " (quantity, price, product_fk, user_fk) 
+            VALUES (:quantity, :price, :product_fk, :user_fk)
         ");
 
         $stmt->bindParam(':product_fk', $productId);
         $stmt->bindParam(':quantity', $quantity );
+        $stmt->bindParam(':price', $price);
+        $stmt->bindParam(':user_fk', $user);
 
         $stmt->execute();
     }
