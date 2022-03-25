c86e339c3ff68bd83114064af93e0ecd185a161c
angel <dehainaut.angelique@orange.fr>
2022-03-25 15:56:23 +0100

    display image according to categories

--- View/category/animals.html.php
+++ View/category/animals.html.php
@@ -4,10 +4,6 @@ use App\Model\Entity\Product;
 
 if (isset($data['products'])) {
     $products = $data['products'];
-    echo '<pre>';
-    var_dump($products);
-    echo '</pre>';
-    die();
 }
 
 foreach ($products as $product) {
