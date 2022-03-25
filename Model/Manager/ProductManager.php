8e426d7918f5d2d1650d47b6fce1a0d905d548cd
angel <dehainaut.angelique@orange.fr>
2022-03-25 14:51:05 +0100

    function productByCategory

--- Model/Manager/ProductManager.php
+++ Model/Manager/ProductManager.php
@@ -9,6 +9,7 @@ use DB_Connect;
 class ProductManager extends AbstractManager
 {
     public const TABLE = "mdf58_product";
+
     public static function getAll() :array
     {
         $images = [];
@@ -28,6 +29,29 @@ class ProductManager extends AbstractManager
         return $images;
     }
 
+    /**
+     * @param int $id
+     * @return \App\Model\Entity\AbstractEntity|Product|string
+     */
+    public static function getProductByCategory(int $id)
+    {
+        $query = DB_Connect::dbConnect()->query("
+            SELECT * FROM ". self::TABLE ." WHERE id IN (SELECT product_fk FROM mdf58_category WHERE id = $id);
+        
+        ");
+
+        if ($query) {
+            foreach ($query->fetchAll() as $productData) {
+                $product = (new Product())
+                    ->setId($productData['id'])
+                    ->setProductName($productData['product_name'])
+                    ->setImage($productData['image'])
+                    ->setPrice($productData['price'])
+                    ->setCategory($productData['category_fk']);
+            }
+        }
+        return $product;
+    }
 
     public static function getProductById (int $id) {
         $query = DB_Connect::dbConnect()->query(
