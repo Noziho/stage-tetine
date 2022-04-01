f1588898d524494814864bd9376125b0db781b00
noahd <noah.decroix3@gmail.com>
2022-04-01 14:29:24 +0200

    Print cart for user

--- View/cart/cart.html.php
+++ View/cart/cart.html.php
@@ -1,9 +1,27 @@
 <?php
 
 use App\Model\Entity\Product;
+use App\Model\Manager\CategoryManager;
 
 if (isset($_SESSION['product'])) {
-        echo "<pre>";
-        var_dump($_SESSION['product']);
-        echo "</pre>";
+    foreach ($_SESSION['product'] as $item) {
+        foreach ($item as $product) {
+            /* @var Product $product */?>
+            <div>
+                <div>
+                    <img
+                        src="/assets/img/category/<?= CategoryManager::getCategoryByProduct($product['product']->getId()) ?>/<?= str_replace(' ', '', $product['product']->getImage()) ?>.jpg"
+                        alt="">
+                </div>
+
+                <p>Nom du produit: <?= $product['product']->getProductName() ?></p>
+                <p>Prénom: <?= $product['firstname'] ?></p>
+                <p>Écriture: <?= $product['font_family'] ?></p>
+                <p><?= $product['color'] ?></p>
+                <p>Embout: <?= $product['tips'] ?></p>
+                <p>Prix: <?= $product['product']->getprice() ?>€</p>
+            </div>
+            <?php
+        }
+    }
 }
