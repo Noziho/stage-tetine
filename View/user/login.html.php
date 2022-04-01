399937d4cb533d13f9c4089472ffe62ba94ac72b
angel <dehainaut.angelique@orange.fr>
2022-04-01 14:55:06 +0200

    responsive login

--- View/user/login.html.php
+++ View/user/login.html.php
@@ -1,7 +1,8 @@
-<div class="container">
+<div class="container" id="login">
     <div id="login-container">
-        <form action="/?c=user&a=login" method="post">
+        <img id="background-login" src="/assets/img/background/background-login2.png" alt="">
 
+        <form action="/?c=user&a=login" method="post">
             <div>
                 <label for="mail">Adresse-mail</label>
                 <input type="email" name="mail" id="mail" minlength="5" maxlength="150" required>
@@ -12,7 +13,9 @@
                 <input type="password" name="password" id="password" minlength="7" maxlength="70" required>
             </div>
             <input value="Se connecter" class="submit-button" type="submit" name="submit">
-
         </form>
+
+        <img id="background-login" src="/assets/img/background/background-login2.png" alt="">
+
     </div>
 </div>
