043310b86cfc211ff8c676f43155da1837bc2eaa
angel <dehainaut.angelique@orange.fr>
2022-04-03 12:51:48 +0200

    means of payment

--- View/cart/cart.html.php
+++ View/cart/cart.html.php
@@ -22,9 +22,26 @@ if (isset($_SESSION['product'])) {
                 <p>Embout: <?= $product['tips'] ?></p>
                 <p>Prix: <?= $product['product']->getprice() ?>€</p>
             </div>
+
+            <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                <input type="hidden" name="cmd" value="_s-xclick">
+                <input type="hidden" name="hosted_button_id" value="XW4XB9HLKH4W8">
+                <table>
+                    <tr><td><input type="hidden" name="on0" value="Tétine">Tétine</td></tr><tr><td><select name="os0">
+                                <option value="1 Unité">1 Unité €9,00 EUR</option>
+                                <option value="2 Unités">2 Unités €16,00 EUR</option>
+                                <option value="3 Unités">3 Unités €22,00 EUR</option>
+                            </select> </td></tr>
+                </table>
+                <input type="hidden" name="currency_code" value="EUR">
+                <input type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal, le réflexe sécurité pour payer en ligne">
+                <img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
+            </form>
+
             <?php
         }
     }
+
 }
 else {?>
     <div id="cartContainerEmpty">
