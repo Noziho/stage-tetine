9f7648b06f5c7b4af2a29a5b33dd7935116ceb0e
noahd <noah.decroix3@gmail.com>
2022-04-06 09:26:07 +0200

    fix cart view + edit database schema

--- View/cart/cart.html.php
+++ View/cart/cart.html.php
@@ -28,7 +28,15 @@ if (isset($_SESSION['product'])) {
                     <button class="submit-button" id="delete_cart_product"><a
                                 href="/index.php?c=basket&a=delete-cart&id=<?= $product['product']->getId() ?>">Supprimer</a>
                     </button>
-                    <!-- Replace "test" with your own sandbox Business account app client ID -->
+                </div>
+
+            </div>
+
+
+            <?php
+        }
+    }?>
+     <!-- Replace "test" with your own sandbox Business account app client ID -->
                     <script src="https://www.paypal.com/sdk/js?client-id=test&currency=USD"></script>
                     <!-- Set up a container element for the button -->
                     <div id="paypal-button-container"></div>
@@ -51,6 +59,9 @@ if (isset($_SESSION['product'])) {
                                     console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
                                     const transaction = orderData.purchase_units[0].payments.captures[0];
                                     alert(`Transaction ${transaction.status}: ${transaction.id}\n\nSee console for all available details`);
+                                    if (transaction.status === "COMPLETED") {
+
+                                    }
                                     // When ready to go live, remove the alert and show a success message within this page. For example:
                                     // const element = document.getElementById('paypal-button-container');
                                     // element.innerHTML = '<h3>Thank you for your payment!</h3>';
@@ -58,16 +69,10 @@ if (isset($_SESSION['product'])) {
                                 });
                             }
                         }).render('#paypal-button-container');
-                    </script>
-                </div>
+                    </script><?php
+}
 
-            </div>
-
-
-            <?php
-        }
-    }
-} else { ?>
+else { ?>
     <div class="emptyCartContainer">
         <div>
             <p>Votre panier est vide.</p>
