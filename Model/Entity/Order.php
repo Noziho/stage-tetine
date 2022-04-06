85cea83f380d3f2fbb817218b98cc0ae396c713c
noahd <noah.decroix3@gmail.com>
2022-04-06 09:33:05 +0200

    add order/OrderProduct entity

--- Model/Entity/Order.php
+++ Model/Entity/Order.php
@@ -4,5 +4,24 @@ namespace App\Model\Entity;
 
 class Order extends AbstractEntity
 {
+    private User $user_fk;
+
+    /**
+     * @return User
+     */
+    public function getUserFk(): User
+    {
+        return $this->user_fk;
+    }
+
+    /**
+     * @param User $user_fk
+     */
+    public function setUserFk(User $user_fk): self
+    {
+        $this->user_fk = $user_fk;
+        return $this;
+    }
+
 
 }
